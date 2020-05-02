87b3aa2f9869085c42852679f98d7469b7a8da6d
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-05-02 21:24:40 +0100

    Simple JS Frontend Workflow with Parcel learned from https://www.youtube.com/watch?v=8rD9amRSOQY

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  const template = `
+    <header>
+    <h1>My Parcel App</h1>
+    <p>
+    Boilertplate frontend CSS workflow using Parcel, Babel and Sass learned from
+    https://www.youtube.com/watch?v=8rD9amRSOQY
+    </p>
+    </header>
+    `;
+
+  return template;
+};
+
+export default Header;
